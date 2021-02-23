639f10ed76ad65c11bac1ffd6942e1732eadf576
acnuns <73796685+acnuns@users.noreply.github.com>
2021-02-23 17:18:17 -0600

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       Weather App!!
       <footer>
-        This project was coded by
-        <a href="https://github.com/acnuns/react-final" target="_blank">Amanda Nunley</a>
+        This project was coded by 
+        <a href="https://github.com/acnuns/react-final" target="_blank"> Amanda Nunley</a>
       </footer>    
+      </div>
     </div>
   );
 }
