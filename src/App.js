1f267f1fd41f79bf37c90a40f4df941f78bd61d3
acnuns <73796685+acnuns@users.noreply.github.com>
2021-02-23 16:51:03 -0600

    Added the Github URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       Weather App!!
+      <footer>
+        This project was coded by
+        <a href="https://github.com/acnuns/react-final" target="_blank">Amanda Nunley</a>
+      </footer>    
     </div>
   );
 }
