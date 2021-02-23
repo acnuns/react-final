0b7c3327f80bac68d2ce4f13b6bacfa5adabce0f
acnuns <73796685+acnuns@users.noreply.github.com>
2021-02-23 17:23:17 -0600

    Updating Weather.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
+import Weather from "./Weather"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-      Weather App!!
+      Weather App
+      <Weather />
       <footer>
         This project was coded by 
         <a href="https://github.com/acnuns/react-final" target="_blank"> Amanda Nunley</a>
